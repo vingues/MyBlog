0e50417d7c768ca3622799cb0a45355362c49292
vingues <1677257556@qq.com>
2026-06-14 12:00:17 +0800

    配置GitHub Pages部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://vingues.github.io',
+  base: '/MyBlog',
   markdown: {
     shikiConfig: {
       theme: 'github-light',
